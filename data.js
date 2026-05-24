461943dd61988f48f957c698707bc4b51999d388
Akmalzz12 <makmalapriyansyah@gmail.com>
2026-05-24 17:43:19 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,150 @@
+const lastUpdate =
+"Update terakhir 23 Mei 2026";
+
+const daftarBulan = [
+
+  "Mei 2026",
+  "Juni 2026"
+
+];
+
+const transaksi = {
+
+  "Mei 2026": [
+
+    {
+      tipe: "pemasukan",
+      title: "Saldo Kas Mushollah",
+      tanggal: "01 Mei 2026",
+      nominal: 1615000
+    },
+    {
+      tipe: "pemasukan",
+      title: "Infaq Dari Toko SB",
+      tanggal: "08 Mei 2026",
+      nominal: 250000
+    },
+    {
+      tipe: "pemasukan",
+      title: "Infaq Dari Bu Hj. Neneng",
+      tanggal: "08 Mei 2026",
+      nominal: 200000
+    },
+    {
+      tipe: "pemasukan",
+      title: "Infaq Uang Kas RT",
+      tanggal: "12 Mei 2026",
+      nominal: 250000
+    },
+    {
+      tipe: "pemasukan",
+      title: "Infaq Dari Ustadz Nur",
+      tanggal: "22 Mei 2026",
+      nominal: 200000
+    },
+    {
+      tipe: "pemasukan",
+      title: "Kotak amal halaman",
+      tanggal: "24 Mei 2026",
+      nominal: 2917000
+    },
+    {
+      tipe: "pemasukan",
+      title: "Kotak amal depan",
+      tanggal: "24 Mei 2026",
+      nominal: 1070000
+    },
+
+    {
+      tipe: "pengeluaran",
+      title: "Insentif Marbot",
+      tanggal: "04 Mei 2026",
+      nominal: 250000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Sabun cuci tangan 1 galon",
+      tanggal: "10 Mei 2026",
+      nominal: 35000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Sabun cuci piring",
+      tanggal: "10 Mei 2026",
+      nominal: 10000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Sabun rinso 6 pcs",
+      tanggal: "10 Mei 2026",
+      nominal: 6000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Wipol Karbol 2 Pcs",
+      tanggal: "10 Mei 2026",
+      nominal: 36000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Busa cuci piring",
+      tanggal: "10 Mei 2026",
+      nominal: 3000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Fostex 1 botol",
+      tanggal: "10 Mei 2026",
+      nominal: 22000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Sitrun 6 pcs",
+      tanggal: "10 Mei 2026",
+      nominal: 18000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Tissue jelly 2 pcs",
+      tanggal: "10 Mei 2026",
+      nominal: 20000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Baterai Alkaline 1 pack",
+      tanggal: "10 Mei 2026",
+      nominal: 16000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Konsumsi Majelis Taklim",
+      tanggal: "10 Mei 2026",
+      nominal: 600000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Tagihan air pam",
+      tanggal: "14 Mei 2026",
+      nominal: 10000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Tagihan internet (Wifi)",
+      tanggal: "14 Mei 2026",
+      nominal: 255000
+    },
+    {
+      tipe: "pengeluaran",
+      title: "Tagihan listrik",
+      tanggal: "14 Mei 2026",
+      nominal: 385000
+    }
+
+  ],
+
+  "Juni 2026": [
+
+  ]
+
+
+};
