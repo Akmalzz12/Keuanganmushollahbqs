f646a62f4d8ec9d3e1193f2292610bba8b338be2
Akmalzz12 <makmalapriyansyah@gmail.com>
2026-06-26 22:50:10 +0700

    Update data.js

--- data.js
+++ data.js
@@ -16,152 +16,152 @@ const transaksi = {
       tipe: "pemasukan",
       title: "Saldo Kas Mushollah",
       tanggal: "01 Mei 2026",
-      nominal: 1.615.000
+      nominal: "1.615.000"
     },
     {
       tipe: "pemasukan",
       title: "Infaq Dari Toko SB",
       tanggal: "08 Mei 2026",
-      nominal: 250.000
+      nominal: "250.000"
     },
     {
       tipe: "pemasukan",
       title: "Infaq Dari Bu Hj. Neneng",
       tanggal: "08 Mei 2026",
-      nominal: 200.000
+      nominal: "200.000"
     },
     {
       tipe: "pemasukan",
       title: "Infaq Uang Kas RT",
       tanggal: "12 Mei 2026",
-      nominal: 250.000
+      nominal: "250.000"
     },
     {
       tipe: "pemasukan",
       title: "Infaq Dari Ustadz Nur",
       tanggal: "22 Mei 2026",
-      nominal: 200.000
+      nominal: "200.000"
     },
     {
       tipe: "pemasukan",
       title: "Kotak amal halaman",
       tanggal: "24 Mei 2026",
-      nominal: 2.917.000
+      nominal: "2.917.000"
     },
     {
       tipe: "pemasukan",
       title: "Kotak amal depan",
       tanggal: "24 Mei 2026",
-      nominal: 1.070.000
+      nominal: "1.070.000"
     },
 
     {
       tipe: "pengeluaran",
       title: "Insentif Marbot",
       tanggal: "04 Mei 2026",
-      nominal: 250.000
+      nominal: "250.000"
     },
     {
       tipe: "pengeluaran",
       title: "Sabun cuci tangan 1 galon",
       tanggal: "10 Mei 2026",
-      nominal: 35.000
+      nominal: "35.000"
     },
     {
       tipe: "pengeluaran",
       title: "Sabun cuci piring",
       tanggal: "10 Mei 2026",
-      nominal: 10.000
+      nominal: "10.000"
     },
     {
       tipe: "pengeluaran",
       title: "Sabun rinso 6 pcs",
       tanggal: "10 Mei 2026",
-      nominal: 6.000
+      nominal: "6.000"
     },
     {
       tipe: "pengeluaran",
       title: "Wipol Karbol 2 Pcs",
       tanggal: "10 Mei 2026",
-      nominal: 36.000
+      nominal: "36.000"
     },
     {
       tipe: "pengeluaran",
       title: "Busa cuci piring",
       tanggal: "10 Mei 2026",
-      nominal: 3.000
+      nominal: "3.000"
     },
     {
       tipe: "pengeluaran",
       title: "Fostex 1 botol",
       tanggal: "10 Mei 2026",
-      nominal: 22.000
+      nominal: "22.000"
     },
     {
       tipe: "pengeluaran",
       title: "Sitrun 6 pcs",
       tanggal: "10 Mei 2026",
-      nominal: 18.000
+      nominal: "18.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tissue jelly 2 pcs",
       tanggal: "10 Mei 2026",
-      nominal: 20.000
+      nominal: "20.000"
     },
     {
       tipe: "pengeluaran",
       title: "Baterai Alkaline 1 pack",
       tanggal: "10 Mei 2026",
-      nominal: 16.000
+      nominal: "16.000"
     },
     {
       tipe: "pengeluaran",
       title: "Konsumsi Majelis Taklim",
       tanggal: "10 Mei 2026",
-      nominal: 600.000
+      nominal: "600.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tagihan air pam",
       tanggal: "14 Mei 2026",
-      nominal: 10.000
+      nominal: "10.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tagihan internet (Wifi)",
       tanggal: "14 Mei 2026",
-      nominal: 255.000
+      nominal: "255.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tagihan listrik",
       tanggal: "14 Mei 2026",
-      nominal: 385.000
+      nominal: "385.000"
     },
     {
       tipe: "pengeluaran",
       title: "Konsumsi kerja bakti",
       tanggal: "24 Mei 2026",
-      nominal: 185.000
+      nominal: "185.000"
     },
     {
       tipe: "pengeluaran",
       title: "Beli tissue jelly 1 pack",
       tanggal: "24 Mei 2026",
-      nominal: 10.000
+      nominal: "10.000"
     },
     {
       tipe: "pengeluaran",
       title: "Beli kantong plastik",
       tanggal: "24 Mei 2026",
-      nominal: 7.000
+      nominal: "7.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tambahan bayar ustdz Fatih",
       tanggal: "24 Mei 2026",
-      nominal: 50.000
+      nominal: "50.000"
     }
 
   ],
@@ -172,85 +172,85 @@ const transaksi = {
       tipe: "pemasukan",
       title: "Infaq Dari Bu Hj. Neneng",
       tanggal: "03 Juni 2026",
-      nominal: 250.000
+      nominal: "250.000"
     },
     {
       tipe: "pemasukan",
       title: "Infaq Uang Kas RT",
       tanggal: "11 Juni 2026",
-      nominal: 250.000
+      nominal: "250.000"
     },
     {
       tipe: "pemasukan",
       title: "Infaq Dari Toko SB",
       tanggal: "12 Juni 2026",
-      nominal: 250.000
+      nominal: "250.000"
     },
     {
       tipe: "pengeluaran",
       title: "Insentif Marbot",
       tanggal: "04 Juni 2026",
-      nominal: 250.000
+      nominal: "250.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tagihan Air PAM",
       tanggal: "11 Juni 2026",
-      nominal: 8.000
+      nominal: "8.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tagihan Internet (Wifi)",
       tanggal: "11 Juni 2026",
-      nominal: 254.000
+      nominal: "254.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tagihan Listrik",
       tanggal: "11 Juni 2026",
-      nominal: 530.000
+      nominal: "530.000"
     },
     {
       tipe: "pengeluaran",
       title: "Tissue Jelly 2 Pcs",
       tanggal: "14 Juni 2026",
-      nominal: 20.000
+      nominal: "20.000"
     },
     {
       tipe: "pengeluaran",
       title: "Baterai Alkaline 2 Pack",
       tanggal: "14 Juni 2026",
-      nominal: 16.000
+      nominal: "16.000"
     },
     {
       tipe: "pengeluaran",
       title: "Sabun Rinso 5 Pcs",
       tanggal: "22 Juni 2026",
-      nominal: 5.000
+      nominal: "5.000"
     },
     {
       tipe: "pengeluaran",
       title: "Wipol Carbol 2 Pcs",
       tanggal: "22 Juni 2026",
-      nominal: 36.000
+      nominal: "36.000"
     },
     {
       tipe: "pengeluaran",
       title: "Sitrun 6 Pcs",
       tanggal: "22 Juni 2026",
-      nominal: 18.000
+      nominal: "18.000"
     },
     {
       tipe: "pengeluaran",
       title: "Kopi Kapal Api 1 Pack",
       tanggal: "22 Juni 2026",
-      nominal: 25.000
+      nominal: "25.000"
     },
     {
       tipe: "pengeluaran",
       title: "Gula Pasir 1KG",
       tanggal: "22 Juni 2026",
-      nominal: 20.000
+      nominal: "20.000"
     }
 
   ],
